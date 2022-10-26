e720afb33a195b1df18da02fc93ef9282b3a7177
Teodoro <teo.calle.lara@gmail.com>
2022-10-26 01:49:34 -0500

    Initial Commit

---
+++ Learning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Learning
+//
+//  Created by TEO on 26/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
